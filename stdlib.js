30ca3315290010898351ce7237f619be4f4d8342
Udit Karode <udit.karode@gmail.com>
2021-10-10 15:57:22 +0530

    [fix:Transpiler/Backends/JS] fix typo

---
+++ stdlib.js
@@ -0,0 +1,11 @@
+const __plus__int = (...args) => args.reduce((acc, curr) => acc + curr);
+const __plus__float = (...args) => Math.round(__plus_int());
+
+const __minus__int = (...args) => args.reduce((acc, curr) => acc + curr);
+const __minus__float = (...args) => args.reduce((acc, curr) => acc + curr);
+
+const __multiply__int = (...args) => args.reduce((acc, curr) => acc + curr);
+const __multiply__float = (...args) => args.reduce((acc, curr) => acc + curr);
+
+const __divide__int = (...args) => args.reduce((acc, curr) => acc + curr);
+const __divide__float = (...args) => args.reduce((acc, curr) => acc + curr);
