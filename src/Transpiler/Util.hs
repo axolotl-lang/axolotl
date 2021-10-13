f7ade741050c4935e5995bde7046add9e1bf7f22
Udit Karode <udit.karode@gmail.com>
2021-10-13 14:55:05 +0530

    [cleanup:Transpiler/Util] use `intersperse` to add commas

--- src/Transpiler/Util.hs
+++ src/Transpiler/Util.hs
@@ -1,6 +1,7 @@
 module Transpiler.Util where
 
 import Data.Foldable (foldlM)
+import Data.List (intersperse)
 import Data.Maybe (fromJust, fromMaybe)
 import qualified Data.Text as T
 
@@ -10,15 +11,5 @@ repeatText t n = foldl (<>) "" (map (const t) [1 .. n])
 makeCommaSep :: T.Text -> [T.Text] -> T.Text -> T.Text
 makeCommaSep header exprs footer =
   header
-    <> fst
-      ( fromMaybe
-          ("", undefined)
-          ( T.unsnoc
-              ( foldl
-                  (\acc curr -> acc <> curr <> ",")
-                  ""
-                  exprs
-              )
-          )
-      )
+    <> foldl (<>) "" (intersperse ", " exprs)
     <> footer
