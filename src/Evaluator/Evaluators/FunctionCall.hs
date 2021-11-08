0212ebbd8d8d61c76e297c32866bbaa0b1a3bab1
Udit Karode <udit.karode@gmail.com>
2021-11-08 16:53:44 +0530

    [feat:Evaluators/FunctionCall] consider variadic arguments

--- src/Evaluator/Evaluators/FunctionCall.hs
+++ src/Evaluator/Evaluators/FunctionCall.hs
@@ -5,15 +5,19 @@ import Analyser.Util as AU
     GDefs,
     LDefs,
     hUnion',
+    rFoldl,
   )
 import Data.HashTable.IO as H (lookup)
 import Data.Maybe (fromJust)
 import qualified Data.Text as T
-import Parser.Ast (Expr (AnonymousFunction), VDataType)
+import Parser.Ast (Expr (AnonymousFunction, Array), VDataType)
 
 type EvaluateExpressionFn = AU.GDefs -> AU.LDefs -> Expr -> IO Expr
 
 type EvalNativeFn = T.Text -> VDataType -> [Expr] -> IO Expr
+
+rFoldl1 :: Foldable t => t a -> (a -> a -> a) -> a
+rFoldl1 list fun = foldl1 fun list
 
 evaluateFunctionCall ::
   AU.GDefs ->
@@ -33,7 +37,24 @@ evaluateFunctionCall gd ld name evaluateExpression argExprs evalNative = do
     -- this is a function
     AU.Function vdt args exprs False -> do
       argExprs' <- mapM (evaluateExpression gd ld) argExprs
-      let unionArgs = zipWith (\d a -> (fst a, AU.Variable (snd a) d)) argExprs' (fst args)
+      let args' = fst args
+      let variadic = snd args
+      let arglen = length args'
+
+      let unionArgs =
+            zipWith3
+              ( \i d a -> do
+                  ( fst a,
+                    AU.Variable
+                      (snd a)
+                      if i == arglen && variadic
+                        then Array (drop arglen argExprs')
+                        else d
+                    )
+              )
+              [1 ..]
+              argExprs'
+              args'
 
       v' <- unionArgs `hUnion'` fromJust defs
       evaluateExpression v' ld $ AnonymousFunction vdt args exprs
